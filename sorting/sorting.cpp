c4c6241b18d22bf809166cbd788ac9017ccece31
DylanRijnen <d.j.f.rijnen@student.tue.nl>
2016-05-13 16:24:20 +0200

    FF wat testen en shizzle

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -17,7 +17,7 @@ using namespace std::chrono;
 //gnuqsort(char *base_ptr, int total_elems, int size, int(*cmp)());
 
 // Initialize the values array and calculate the number of elements in the array "divide the total size of the array by the size of the array element"
-int values[10];
+int values[10000000];
 int length = sizeof(values) / sizeof(int);
 
 int cmpfunc(const void * a, const void * b)
@@ -25,7 +25,7 @@ int cmpfunc(const void * a, const void * b)
 	return (*(int*)a - *(int*)b);
 }
 
-int gnuqsort(int *base_ptr, int total_elems, int size, int(*cmp)());
+// int gnuqsort(int *base_ptr, int *total_elems, int *size, int(*cmp)());
 
 int main()
 {
@@ -52,9 +52,9 @@ int main()
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
 	//qsort
-	//qsort(values, length, sizeof(int), cmpfunc);
+	qsort(values, length, sizeof(int), cmpfunc);
 
-	gnuqsort(values, length, sizeof(int), cmpfunc);
+	//gnuqsort(values, length, sizeof(int), cmpfunc);
 
 	//timsort
 
@@ -67,11 +67,11 @@ int main()
 
 
 	// Optional printing of the array after sorting
-	printf("\nAfter sorting the list is: \n");
-	for (n = 0; n < length; n++)
-	{
-		printf("%d ", values[n]);
-	}
+	//printf("\nAfter sorting the list is: \n");
+	//for (n = 0; n < length; n++)
+	//{
+	//	printf("%d ", values[n]);
+	//}
 
 	//Calculate execution time
 	auto duration = duration_cast<microseconds>(t2 - t1).count();
