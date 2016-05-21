8ea77f4dc908a63d268d05f9b0c3740de6262c42
Thomas Hagebols <thomashagebols@hotmail.com>
2016-05-21 17:58:25 +0200

    fprintf commented

--- sorting/sorting.cpp
+++ sorting/sorting.cpp
@@ -15,17 +15,18 @@ using namespace std::chrono;
 //gnuqsort(char *base_ptr, int total_elems, int size, int(*cmp)());
 
 // Initialize the arrays which need to be sorted and calculate the number of elements in the array "divide the total size of the array by the size of the array element"
-int unsorted[1000];
-int copyUnsorted[1000];
-int length = sizeof(unsorted) / sizeof(int);
+long long unsorted[100];
+long long copyUnsorted[100];
+int length = sizeof(unsorted) / sizeof(long long);
 
-int patsort(int values[], int length);
+int patsort(long long values[], int length);
+int patsortplus(long long values[], int length);
 // int patsortplus(int values[], int length);
 
 
 int cmpfunc(const void * a, const void * b)
 {
-	return (*(int*)a - *(int*)b);
+	return (*(long long*)a - *(long long*)b);
 }
 
 // int gnuqsort(int *base_ptr, int *total_elems, int *size, int(*cmp)());
@@ -42,16 +43,16 @@ int main()
 	}
 
 	// Perform different sorting algorithms and time the runtime
-	for (int i{0}; i < 2; i++)
+	for (int i{0}; i < 3; i++)
 	{
 		std::copy(unsorted, unsorted + length, copyUnsorted);
 
 		// Optional printing of the generated array before sorting
-		//printf("before sorting the list is: \n");
-		//for (int n{0}; n < length; n++)
-		//{
-		//	printf("%d ", copyUnsorted[n]);
-		//}
+		printf("before sorting the list is: \n");
+		for (int n{0}; n < length; n++)
+		{
+			printf("%d ", copyUnsorted[n]);
+		}
 		
 		high_resolution_clock::time_point t1 = high_resolution_clock::now();
 		switch (i)
@@ -61,41 +62,47 @@ int main()
 			break;
 		case 0:
 			//qsort
+			cout << "qsort:" << endl;
 			qsort(copyUnsorted, length, sizeof(int), cmpfunc);
 			//gnuqsort(copyUnsorted, length, sizeof(int), cmpfunc);
 			break;
 		case 1:
 			//Patience sort
+			cout << "Patience sort:" << endl;
  			patsort(copyUnsorted, length);
 			//patsortplus(copyUnsorted, length);
 			break;
 		case 2:
 			//Patience+ sort
-
+			cout << "Patient+ sort:" << endl;
+			patsortplus(copyUnsorted, length);
 			break;
 		case 3:
 			//P3 sort
+			cout << "P3 sort:" << endl;
+
 			break;
 		case 4:
 			//TimSort
+			cout << "Timsort:" << endl;
 			break;
 		default:
 			// i undefined
-			cout << "Switch out of bound" << endl;
+			cout << "Switch out of bound:" << endl;
 			break;
 		}
 		high_resolution_clock::time_point t2 = high_resolution_clock::now();
 
 		// Optional printing of the array after sorting
-		//printf("\nAfter sorting the list is: \n");
-		//for (int n{0}; n < length; n++)
-		//{
-		//	printf("%d ", copyUnsorted[n]);
-		//}
+		printf("\nAfter sorting the list is: \n");
+		for (int n{0}; n < length; n++)
+		{
+			printf("%d ", copyUnsorted[n]);
+		}
 
 		//Calculate and print execution time
 		auto duration = duration_cast<microseconds>(t2 - t1).count();
-		printf("\nTime needed for sorting: %d microseconds ", duration);
+		printf("\nTime needed for sorting: %d microseconds \n\n", duration);
 	}
 
     return 0;
