a9c5c7781f25b8d8db0ecfb5c3328a0d43003650
DylanRijnen <d.j.f.rijnen@student.tue.nl>
2016-05-31 16:44:08 +0200

    Revert "Added some improvements to the code of Ping Pong merge + some ideas"
    This reverts commit 346e97a7f2a7c2e08d93e3a4ba11536a3a43eaaf.

--- sorting/PingPong.cpp
+++ sorting/PingPong.cpp
@@ -26,19 +26,16 @@ int insertArray(long long elemArray[], long long pile[], int runSize, int nextEm
 	}
 	return 0;
 }
-// where first = runs[] and last = runs[] + sizeof(runs[])/sizeof(runs[0]) this only works when all runs are equally sized............... otherwise the number of runs needs to be stored seperately and passed
-int UPingPongMerge(Iterator first, int numberOfRuns, int runSize[], const length)
+
+int UPingPongMerge(long long runs[], int runSize[], const length)
 {
-	Iterator last;
-	last = first + numberOfRuns;
 	//initialize array with number of integers
 	long long elems1[length];
 	long long elems2[length];
 	vector<pair<int, int>> runSizeRef;
 	//fill array with index and sizes
 	for(Iterator it = first; it != last; it++){
-		long long& element = *it;
-		runSizeRef.push_back(make_pair( element, runSize[it]));
+		runSizeRef.push_back(make_pair( it, runSize[it]));
 	}
 
 	//sort array ascending sizes
@@ -48,11 +45,10 @@ int UPingPongMerge(Iterator first, int numberOfRuns, int runSize[], const length
 	
 	//still wrong insertarray and elemsrun
 	vector<tuple<int,int,int>> elemsRun;
-	// opmerking van Dylan: in C++ moet je sizeof gebruiken en dan krijg je byte size van de array, vervolgens wil je dit
-	// delen door de size van een element in de array verder wil je wel de elements zelf in elems1 knallen
-	for(int j = {0}; j < sizeof(runSizeRef)/sizeof(runSizeRef[0]); j++){
-		copy(runs[j], runs[j] + runSize[j], elems1[nextEmptyArrayLoc]);
+	for(int j = {0}; j < runSizeRef.size(); j++){
+		insertArray(elems1, runs[j], runSize[j], nextEmptyArrayLoc);
 		elemsRun.push_back(make_tuple(1,nextEmptyArrayLoc,runSize[j]));
+		//also worong
 		nextEmptyArrayLoc += runSize[j];
 	}
 	
@@ -81,7 +77,6 @@ int UPingPongMerge(Iterator first, int numberOfRuns, int runSize[], const length
 		//26 if (ElemsRuns.First.RunIndex == 1) return Elems1
 		//27 else return Elems2
 	}
-
 	
 
 //16 if (curRun has no next) or
